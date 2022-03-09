97b113fb4ef51572dd89b94325f0dc708ae91814
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2022-03-09 01:09:01 -0800

    Testing

--- club/tests.py
+++ club/tests.py
@@ -37,3 +37,19 @@ class MeetingType_Form_Test(TestCase):
         form=MeetingTypeForm(data={'meetingTitle': "party", 'meetingAgenda' : "something"})
         self.assertTrue(form.is_valid())
 
+class New_Meeting_authentication_test(TestCase):
+    def setUp(self):
+        self.test_user=User.objects.create_user(username='testuser1', password='P@ssw0rd1')
+        self.type=meetingTitle.objects.create(meetingTitle='Party')
+        self.prod = Meeting.objects.create(meetingTitle='The Party', meetingDate='2022-04-02', meetingTime='2pm' user=self.test_user, meetingLocation='DownTown', meetingAgenda='Have a good Time!')
+
+    def test_redirect_if_not_logged_in(self):
+        response=self.client.get(reverse('newmeeting'))
+        self.assertRedirects(response, '/accounts/login/?next=/club/newMeeting/')
+
+    def test_Logged_in_uses_correct_template(self):
+        login=self.client.login(username='testuser1', password='P@ssw0rd1')
+        response=self.client.get(reverse('newmeeting'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club/newmeeting.html')
